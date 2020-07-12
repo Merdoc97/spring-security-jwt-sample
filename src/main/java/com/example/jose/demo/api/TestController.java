68bca960726f7c1e1949e174473f8c5280fdb045
merdoc97@gmail.com <merdoc97@gmail.com>
2020-07-12 17:25:24 +0300

    jwt example without keycloak adapter

--- src/main/java/com/example/jose/demo/api/TestController.java
+++ src/main/java/com/example/jose/demo/api/TestController.java
@@ -1,18 +1,20 @@
 package com.example.jose.demo.api;
 
-import org.springframework.security.core.Authentication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class TestController {
 
     @GetMapping(value = "/me")
-    public Authentication get(Authentication authentication) {
-
-        System.out.println("principal name is " + authentication.getName());
+    public Jwt get(final @AuthenticationPrincipal Jwt authentication) {
+        log.info("user name is {}",authentication.getClaimAsString("preferred_username"));
         return authentication;
     }
 }
